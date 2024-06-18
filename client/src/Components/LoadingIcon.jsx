312df982abbec83513ecdf07c0acbbff62243356
shuu <shuux.dev@gmail.com>
2024-06-18 15:42:59 +0700

    Added a loading spinner component.

---
+++ client/src/Components/LoadingIcon.jsx
@@ -0,0 +1,5 @@
+export const LoadingIcon = () => {
+    return (
+        <div className="animate-spin rounded-full h-20 w-20 border-t-4 border-l-4 border-r-4 border-b-4 border-gray-900 border-r-white"></div>
+    );
+};
